179d4b2ad469f41e44939f6fed5908efb2135f6d
leuzaoo <dev.lcostaoliveira@gmail.com>
2023-11-20 14:11:01 -0300

    Updated sidebar

--- app/components/sidebar/index.tsx
+++ app/components/sidebar/index.tsx
@@ -1,5 +1,12 @@
+"use client"
 import React from "react";
+import styled from "styled-components";
+import { useGlobalState } from "@/app/context/globalProvider";
+
+const SidebarStyled = styled.nav``;
 
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  const { theme } = useGlobalState();
+  console.log(theme);
+  return <SidebarStyled theme={theme}>Sidebar</SidebarStyled>;
 }
