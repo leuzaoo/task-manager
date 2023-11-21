b8689609e4f4f8fae7477805a3eb7ba3af6c4b20
leuzaoo <dev.lcostaoliveira@gmail.com>
2023-11-21 19:10:08 -0300

    Add new assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* font awesome cdn */}
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+          integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
       <body className={inter.className}>
         <ContextProvider>
           <GlobalStyleProvider>
