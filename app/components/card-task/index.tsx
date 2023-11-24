95a58f0f9699734264f640d17f20e4feff5e32e5
leuzaoo <dev.lcostaoliveira@gmail.com>
2023-11-24 19:17:00 -0300

    Updated

--- app/components/card-task/index.tsx
+++ app/components/card-task/index.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { useGlobalState } from "@/app/context/globalProvider";
-import React from "react";
-import styled from "styled-components";
 import CreateContent from "../modals/CreateContent";
+import styled from "styled-components";
+import React from "react";
 
 type Props = {};
 
